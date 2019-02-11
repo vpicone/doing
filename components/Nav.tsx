8068f0d876a0672b01409acccc3cb3b1f90a196d
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2019-02-11 12:46:36 -0600

    add scroll hook and styles

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,18 +1,28 @@
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import styled from "./styled-components";
 
-const LinkList = styled.ul<{ home: boolean }>`
+type ListProps = {
+  home: boolean;
+  scrolling: string;
+};
+
+const LinkList = styled.ul<ListProps>`
   display: flex;
   justify-content: center;
   margin: 0;
   padding: 0;
+  padding-bottom: 0.5rem;
   position: ${props => (props.home ? "relative" : "fixed")};
   width: 100vw;
   left: 0;
   top: 0;
   background: ${props => (props.home ? "transparent" : "white")};
+  transition: all 0.5s ease;
   box-shadow: ${props =>
-    props.home ? "none" : "0 2px 2px rgba(0, 0, 0, 0.05)"};
+    props.home || props.scrolling == "false"
+      ? "none"
+      : "0 2px 2px rgba(0, 0, 0, 0.05)"};
   a {
     text-decoration: none;
     color: ${props => (props.home ? props.theme.colors.brand03 : "#fb4b53")};
@@ -44,24 +54,32 @@ const ListItem = styled.li`
     padding: 0 0.25rem;
   }
 `;
-const Nav: React.FC<{ home?: boolean }> = ({ home }) => (
-  <nav>
-    <LinkList home={home}>
-      <ListItem>
-        <a href="https://github.com/vpicone">github</a>
-      </ListItem>
-      <ListItem>
-        <a href="https://linkedin.com/in/vincepicone">linkedin</a>
-      </ListItem>
-      <ListItem>
-        <a href="https://twitter.com/TheElodin">twitter</a>
-      </ListItem>
-      <ListItem>
-        <Link href="blog">
-          <a>blog</a>
-        </Link>
-      </ListItem>
-    </LinkList>
-  </nav>
-);
+const Nav: React.FC<{ home?: boolean }> = ({ home }) => {
+  const [scrolling, setScrolling] = useState(false);
+  useEffect(() => {
+    const onScroll = () => setScrolling(window.scrollY > 0);
+    document.addEventListener("scroll", onScroll);
+    return () => document.removeEventListener("scroll", onScroll);
+  });
+  return (
+    <nav>
+      <LinkList home={home} scrolling={scrolling ? "true" : "false"}>
+        <ListItem>
+          <a href="https://github.com/vpicone">github</a>
+        </ListItem>
+        <ListItem>
+          <a href="https://linkedin.com/in/vincepicone">linkedin</a>
+        </ListItem>
+        <ListItem>
+          <a href="https://twitter.com/TheElodin">twitter</a>
+        </ListItem>
+        <ListItem>
+          <Link href="blog">
+            <a>blog</a>
+          </Link>
+        </ListItem>
+      </LinkList>
+    </nav>
+  );
+};
 export default Nav;
